d4c58a0758ad19f5be3bb715de509d0fd1aed2ce
alaamohamed58 <alaamohamed58999@gmail.com>
2022-12-31 23:22:45 +0000

    NEW

---
+++ src/drag.ts
@@ -0,0 +1,128 @@
+let card = document.querySelectorAll("#card > div");
+const addLogoBtn = <HTMLInputElement>document.querySelector("#logoBtn input");
+
+addLogoBtn.addEventListener("change", () => {
+  card = document.querySelectorAll("#card > div");
+  console.log(card);
+});
+let initialX: number = 0,
+  initialY: number = 0;
+let moveElement: boolean = false;
+interface Events {
+  mouse: {
+    up: string;
+    move: string;
+    down: string;
+  };
+
+  touch: {
+    up: string;
+    move: string;
+    down: string;
+  };
+}
+
+//Events object
+let events: Events = {
+  mouse: {
+    up: "mouseup",
+    move: "mousemove",
+    down: "mousedown",
+  },
+
+  touch: {
+    up: "touchend",
+    move: "touchmove",
+    down: "touchstart",
+  },
+};
+
+let deviceType: string = "";
+const isTouchDevice = (): boolean => {
+  try {
+    document.createEvent("TouchEvent");
+    deviceType = "touch";
+    return true;
+  } catch (error) {
+    deviceType = "mouse";
+    return false;
+  }
+};
+
+isTouchDevice();
+
+// mouse down
+export function onMousedown(e?: any): void {
+  //initial x and y
+  initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+  initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+  //initial x and y
+  initialX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+  initialY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+  //START MOVE
+  moveElement = true;
+}
+
+// mouse down
+card.forEach((el: any) => {
+  el.addEventListener(events[deviceType].down, (e: any) => {
+    e.preventDefault();
+    onMousedown(e);
+    el.style.cursor = "grab";
+    el.style.border = "1px dashed black";
+  });
+});
+
+//mouse move
+export function mouseMove(
+  e?: any,
+  el?: {
+    style: { top: string; left: string; cursor: string };
+    offsetTop: number;
+    offsetLeft: number;
+  }
+) {
+  if (moveElement) {
+    e.preventDefault();
+
+    let newX = !isTouchDevice() ? e.clientX : e.touches[0].clientX;
+    let newY = !isTouchDevice() ? e.clientY : e.touches[0].clientY;
+
+    if (el != null) {
+      el.style.top = el.offsetTop - (initialY - newY) + "px";
+      el.style.left = el.offsetLeft - (initialX - newX) + "px";
+      el.style.cursor = "grabbing";
+    }
+    initialX = newX;
+    initialY = newY;
+  }
+}
+
+//move
+card.forEach((el: any) => {
+  el.addEventListener(events[deviceType].move, (e: any) => {
+    mouseMove(e, el);
+  });
+});
+
+function stopMovement(): void {
+  moveElement = false;
+}
+//up
+card.forEach((el: any) => {
+  el.addEventListener(events[deviceType].up, () => {
+    stopMovement();
+    el.style.cursor = "grab";
+    el.style.border = "none";
+  });
+});
+
+card.forEach((el: any) => {
+  el.addEventListener("mouseleave", () => {
+    stopMovement();
+    el.style.cursor = "grab";
+    el.style.border = "none";
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,220 @@
+const sideBarElements: NodeListOf<Element> =
+  document.querySelectorAll("#side-nav li");
+
+sideBarElements.forEach((li: any) => {
+  li.onclick = function () {
+    this.classList.toggle("active");
+  };
+});
+
+/**************************************************************************************** */
+
+//date
+const card_date = document.getElementById("card-date") as HTMLDivElement;
+
+card_date.textContent = new Date().toLocaleDateString();
+
+/**************************************************************************************** */
+
+// Hide Elements
+sideBarElements.forEach(function (li: any): void {
+  li.addEventListener("click", function () {
+    if (this.dataset.inform) {
+      let item = document.querySelector(this.dataset.inform);
+      item.classList.toggle("hide");
+    }
+  });
+});
+
+/**************************************************************************************** */
+// set background
+const card_container = document.getElementById("card") as HTMLDivElement;
+const fileInput = <HTMLInputElement>(
+  document.querySelector("#background-btn input[type = file]")
+);
+//setbackground function
+const inputFileType: string[] = [
+  "image/apng",
+  "image/bmp",
+  "image/gif",
+  "image/jpeg",
+  "image/pjpeg",
+  "image/png",
+  "image/svg+xml",
+  "image/tiff",
+  "image/webp",
+  "image/x-icon",
+];
+function validFileTypes(file: any): any {
+  return inputFileType.includes(file.type);
+}
+function setBackGroundImg(fileInput: HTMLInputElement) {
+  let curFiles = fileInput.files;
+
+  for (const file of curFiles) {
+    if (validFileTypes(file)) {
+      let backgroundImg = URL.createObjectURL(file);
+      card_container.style.backgroundImage = `url(${backgroundImg})`;
+    }
+  }
+}
+fileInput.addEventListener("change", updateImage);
+function updateImage(): void {
+  setBackGroundImg(fileInput);
+}
+//background btn
+const backgroundBtn = document.getElementById(
+  "background-btn"
+) as HTMLLIElement;
+
+backgroundBtn.addEventListener("click", function () {
+  if (!this.classList.contains("active")) {
+    card_container.style.backgroundImage = "unset";
+    fileInput.style.display = "block";
+  }
+  if (this.classList.contains("active")) {
+    fileInput.style.display = "none";
+  }
+});
+
+// change style
+const cardElements: Element[] = Array.from(card_container.children);
+
+const selectedItem = document.getElementById("selected") as HTMLDivElement;
+const fontColor = document.getElementById("fontcolor") as HTMLInputElement;
+const fontSize = document.getElementById("fontsize") as HTMLInputElement;
+const fontBold = document.getElementById("font-bold") as HTMLInputElement;
+const italicFont = document.getElementById("italic") as HTMLInputElement;
+
+//change style
+cardElements.forEach((e: any) => {
+  e.addEventListener("dblclick", function () {
+    fontBold.classList.remove("bold");
+    italicFont.classList.remove("italic");
+    selectedItem.textContent = e.innerText;
+    fontColor.disabled = false;
+    fontSize.disabled = false;
+    fontBold.disabled = false;
+    italicFont.disabled = false;
+
+    e.style.border = `1px dashed black`;
+    changeStyle(e);
+  });
+});
+
+//disable button
+if (selectedItem.textContent === "") {
+  fontColor.disabled = true;
+  fontSize.disabled = true;
+  fontBold.disabled = true;
+  italicFont.disabled = true;
+}
+
+function changeStyle(e: any): void {
+  if (selectedItem.textContent === "") {
+    return;
+  }
+
+  //change color
+
+  fontColor.oninput = () => {
+    e.style.color = `${fontColor.value}`;
+  };
+
+  //change fontSize
+
+  fontSize.oninput = () => {
+    e.style.fontSize = `${fontSize.value}px`;
+  };
+
+  //font bold
+
+  fontBold.onclick = () => {
+    fontBold.classList.toggle("bold");
+    if (fontBold.classList.contains("bold")) {
+      e.style.fontWeight = "bold";
+    } else {
+      e.style.fontWeight = "normal";
+    }
+  };
+
+  //font italic
+
+  italicFont.onclick = () => {
+    italicFont.classList.toggle("italic");
+    if (italicFont.classList.contains("italic")) {
+      e.style.fontStyle = "italic";
+    } else {
+      e.style.fontStyle = "normal";
+    }
+  };
+}
+
+// add logo
+const addLogoBtn = <HTMLInputElement>document.querySelector("#logoBtn input");
+const radiusRange = document.getElementById("radius-size") as HTMLInputElement;
+
+addLogoBtn.addEventListener("change", updateLogo);
+
+function updateLogo(): void {
+  setLogo(addLogoBtn);
+}
+
+function setLogo(fileInput: HTMLInputElement) {
+  let curFiles = fileInput.files;
+  let img: HTMLImageElement = document.createElement("img");
+  let logoContainer = document.getElementById(
+    "logo-container"
+  ) as HTMLDivElement;
+
+  for (const file of curFiles) {
+    while (logoContainer.lastElementChild) {
+      logoContainer.removeChild(logoContainer.lastElementChild);
+    }
+    radiusRange.value = 0;
+    if (validFileTypes(file)) {
+      let logo = URL.createObjectURL(file);
+      card_container;
+      img.src = `${logo}`;
+      img.alt = `logo`;
+      logoContainer.appendChild(img);
+      card_container.appendChild(logoContainer);
+    }
+  }
+}
+
+// chnage logo radius
+
+radiusRange.addEventListener("input", () => {
+  const logo = <HTMLImageElement>document.querySelector("#logo-container img");
+
+  if (!logo) {
+    radiusRange.disabled = true;
+  } else {
+    radiusRange.disabled = false;
+
+    logo.style.borderRadius = `${radiusRange.value}%`;
+  }
+});
+
+radiusRange.closest("li").addEventListener("click", function () {
+  this.classList.toggle("close");
+});
+
+//rotate img
+const rotateRight = document.getElementById("rotate-right") as HTMLLIElement;
+const rotateLeft = document.getElementById("rotate-left") as HTMLLIElement;
+//rotate right
+let rotateRightVal = 0;
+rotateRight.addEventListener("click", () => {
+  const logo = <HTMLImageElement>document.querySelector("#logo-container img");
+  rotateRightVal += 45;
+  logo.style.transform = `rotate(${rotateRightVal}deg)`;
+});
+//rotate left
+let rotateLeftVal = 0;
+rotateLeft.addEventListener("click", () => {
+  const logo = <HTMLImageElement>document.querySelector("#logo-container img");
+  rotateLeftVal -= 45;
+  logo.style.transform = `rotate(${rotateLeftVal}deg)`;
+});
